49acef3a8f30e6b75b525c39b629dd15aa718b1d
Marat <104265985+Maratq@users.noreply.github.com>
2023-06-26 21:02:35 +0300

    .

---
+++ models/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import MetaData, Table, Column, Integer, String, TIMESTAMP, ForeignKey, JSON, null
+from datetime import datetime
+
+metadata = MetaData()
+
+roles = Table(
+    'roles',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String, nullable=False),
+    Column('permissions', JSON),
+)
+
+users = Table(
+    'users',
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('email', String, nullable=False),
+    Column('username', String, nullable=False),
+    Column('password', String, nullable=False),
+    Column('registration_at', TIMESTAMP, default=datetime.utcnow),
+    Column('role_id', Integer, ForeignKey('roles.id')),
+)
