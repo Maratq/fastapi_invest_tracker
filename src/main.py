44a4243ba3505b913595cbd4e393fc2ad457eb52
Marat <104265985+Maratq@users.noreply.github.com>
2023-06-28 15:46:33 +0300

    update file structure, new folder src

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Depends
+from fastapi_users import FastAPIUsers
+
+from src.auth.base_config import auth_backend
+from src.auth.manager import get_user_manager
+from src.auth.schemas import UserRead, UserCreate
+from src.auth.models import User
+
+from src.operations.router import router as router_operation
+
+app = FastAPI(
+    title="Trading App"
+)
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+app.include_router(router_operation)
