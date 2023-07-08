d05adeb4af2c5204cc4fde469678a147c10c6185
Marat <104265985+Maratq@users.noreply.github.com>
2023-07-08 16:21:17 +0300

    Update router.py

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,10 +1,11 @@
 import time
 
 from fastapi import APIRouter, Depends, HTTPException
-from sqlalchemy import select, insert
+from fastapi_cache.decorator import cache
+from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi_cache.decorator import cache
+from src.auth.base_config import current_user
 from src.database import get_async_session
 from src.operations.models import operation
 from src.operations.schemas import OperationCreate
@@ -23,7 +24,10 @@ def get_long_op():
 
 
 @router.get("")
-async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+async def get_specific_operations(
+        operation_type: str,
+        session: AsyncSession = Depends(get_async_session),
+):
     try:
         query = select(operation).where(operation.c.type == operation_type)
         result = await session.execute(query)
@@ -43,10 +47,13 @@ async def get_specific_operations(operation_type: str, session: AsyncSession = D
 
 @router.post("")
 async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
-    """
-    docstring
-    """
     stmt = insert(operation).values(**new_operation.dict())
     await session.execute(stmt)
     await session.commit()
     return {"status": "success"}
+
+
+@router.get("/main")
+async def main(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(1))
+    return result.all()
